0d5f1fb88109ec1b82af8a2b9ef2a05d86fffa55
202043246 Jose Miguel Becerra Casierra/202043291 Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co/brand.juan@correounivalle.edu.co>
2022-01-27 17:54:41 -0500

    Creacion del documento y la clase 'Dados'

---
+++ src/myProject/Dados.java
@@ -0,0 +1,22 @@
+package myProject;
+
+import java.util.Random;
+
+/**
+ * Class Dado generate a random value between 1 and 6
+ * @author Juan Esteban Brand Tovar
+ * @version 1.0.0 date 04/12/2021
+ */
+public class Dados {
+    private int cara;
+
+    /**
+     * Method that generate an random value to "cara"
+     * @return number between (1,6)
+     */
+    public int getCara() {
+        Random aleatorio = new Random();
+        cara = aleatorio.nextInt(6)+1;
+        return cara;
+    }
+}
